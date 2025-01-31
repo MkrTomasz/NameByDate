efb52159f6ca409f69c54a86d2c8f4a4acef5796
MkrTomasz <mkr.tomasz@gmail.com>
2025-01-31 23:05:43 +0100

    Run button added

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -1,29 +1,42 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog
+from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog
+from MainPackage.MetaExtract import MetaProcessor
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.folder_path = "No path selected"
+
         self.setMinimumSize(400, 150)
 
         parentLayout = QVBoxLayout()
 
-        self.label = QLabel("No path selected")
-        self.button = QPushButton("Choose folder path")
+        self.label = QLabel(self.folder_path)
+        self.path_button = QPushButton("Choose folder path")
+        self.run_button = QPushButton("Run")
 
-        self.button.clicked.connect(self.clickButton)
+        self.path_button.clicked.connect(self.clickPathButton)
+        self.run_button.clicked.connect(self.clickRunButton)
 
         parentLayout.addWidget(self.label)
-        parentLayout.addWidget(self.button)
+        parentLayout.addWidget(self.path_button)
+        parentLayout.addWidget(self.run_button)
 
         centerWidget = QWidget()
         centerWidget.setLayout(parentLayout)
         self.setCentralWidget(centerWidget)
 
 
-    def clickButton(self):
-        folder_path = str(QFileDialog.getExistingDirectory(self, "Select folders with files to be soreted"))
-        if folder_path != "":
-            self.label.setText(folder_path)
+    def clickPathButton(self):
+        self.folder_path = str(QFileDialog.getExistingDirectory(self, "Select folders with files to be soreted"))
+        if self.folder_path != "":
+            self.label.setText(self.folder_path)
+            return self.folder_path
         else:
             self.label.setText("No path selected")
+            return self.folder_path
+
+
+    def clickRunButton(self):
+        processor = MetaProcessor(self.folder_path)
+        processor.printDir()
--- MainPackage/MetaExtract.py
+++ MainPackage/MetaExtract.py
@@ -1,53 +1,53 @@
 import os
 from datetime import datetime
 
-#convert from numerical timestamp to date YYYY-MM-DD
-def timeConvert(time_value):
-  time_value = time_value
-  new_time_format = datetime.fromtimestamp(time_value)
-  return new_time_format.date()
 
-#convert size from numerical to KB with 2 decimal places
-def sizeFormat(size):
-    new_size_format = format(size/1024, ".2f")
-    return new_size_format + " KB"
+class MetaProcessor():
+    def __init__(self, source_dir):
+        self.source_dir = source_dir
+
+    #convert from numerical timestamp to date YYYY-MM-DD
+    def timeConvert(self, time_value):
+        time_value = time_value
+        new_time_format = datetime.fromtimestamp(time_value)
+        return new_time_format.date()
+
+    #convert size from numerical to KB with 2 decimal places
+    def sizeFormat(self, size):
+        new_size_format = format(size/1024, ".2f")
+        return new_size_format + " KB"
 
 
-def createFileRecords(source_dir):
+    def createFileRecords(self):
 
-    firstDict = {}
-    
-    for name in os.listdir(source_dir): 
+        firstDict = {}
         
-        filepath = os.path.join(source_dir, name)
+        for name in os.listdir(self.source_dir): 
+            
+            filepath = os.path.join(self.source_dir, name)
+            
+            #main library that holds stats
+            stats = os.stat(filepath)
+            
+            attrs = {
+                'File Name': name,
+                'Size (KB)': self.sizeFormat(stats.st_size),
+                'Creation Date': self.timeConvert(stats.st_birthtime),
+                'Modified Date': self.timeConvert(stats.st_mtime),
+                'Last Access Date': self.timeConvert(stats.st_atime),            
+            }
+                
+            firstDict[name] = attrs 
         
-        #main library that holds stats
-        stats = os.stat(filepath)
+        return firstDict 
+
+
+    def printDir(self):
+        dictOfDicts = self.createFileRecords()
+
+        for n, a in dictOfDicts.items():
         
-        attrs = {
-            'File Name': name,
-            'Size (KB)': sizeFormat(stats.st_size),
-            'Creation Date': timeConvert(stats.st_birthtime),
-            'Modified Date': timeConvert(stats.st_mtime),
-            'Last Access Date': timeConvert(stats.st_atime),            
-        }
-               
-        firstDict[name] = attrs 
-    
-    return firstDict 
-
-
-def printDir(source_dir):
-    dictOfDicts = createFileRecords(source_dir)
-
-    for n, a in dictOfDicts.items():
-    
-        print(f"Displaying for file: '{n}':")
-        for i, j in a.items():
-            print(f"{i}: {j}")
-        print()
-
-
-#if __name__ == "__main__":
-   
-    #printDir('')
+            print(f"Displaying for file: '{n}':")
+            for i, j in a.items():
+                print(f"{i}: {j}")
+            print()
